dd32517f22712f1427c3ee9817c2264ed8a9394d
Paulo Schneider <paulo.schneider@gmail.com>
2017-02-01 18:50:37 +0000

    Basic test for dummy implementation of heap

--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -1,7 +1,19 @@
 require_relative '../src/heap'
 
 describe DumbHeap do
-  it 'should do stuff' do
-    expect(true).to eq(false)
+  subject { DumbHeap.new }
+
+  it 'should have no top when empty' do
+    expect(subject.top).to be_nil
+  end
+
+  it 'should throw exception when poping from empty heap' do
+    # TODO improve style test
+    begin
+      subject.pop
+      expect(true).to eq(false)
+    rescue  => e
+      # fine
+    end
   end
 end
--- src/heap.rb
+++ src/heap.rb
@@ -1,2 +1,10 @@
 class DumbHeap
+
+  def top
+    nil
+  end
+
+  def pop
+    raise "error empty"
+  end
 end
